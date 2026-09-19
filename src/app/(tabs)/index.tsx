d6d821d1d93192b72af3ac8dd8310fd1a9c1bf76
Ryo-rom <r0567280@gmail.com>
2026-09-19 19:01:52 +0900

    Complete 3rd section.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import Button from "@/components/button";
+import ImageViewer from "@/components/image-viewer";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen.
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -17,16 +23,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
 
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
 
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
