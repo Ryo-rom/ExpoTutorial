06617d82d8c041861063c9957526edbc56f06271
Ryo-rom <r0567280@gmail.com>
2026-09-19 16:39:36 +0900

    complete first section.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Home screen</Text>
     </View>
   );
 }
@@ -11,7 +11,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  text: {
+    color: "#fff",
   },
 });
